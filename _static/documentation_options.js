55ac3b12f36de46036247a4f559829575a1444d3
Matthew Newville <newville@cars.uchicago.edu>
2019-09-01 21:10:21 -0500

    blank .gitignore for docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1+24.g225ca49',
+    VERSION: '0.1+24.g225ca49-py3.7.egg',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',

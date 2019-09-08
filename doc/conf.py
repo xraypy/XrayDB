6bf8d539193c441be7ca7c74d571077caa4ea56a
Matthew Newville <newville@cars.uchicago.edu>
2019-09-08 08:32:27 -0500

    clarify license

--- doc/conf.py
+++ doc/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'xraydb'
-copyright = 'Matthew Newville, The University of Chicago, 2019'
+copyright = 'Public Domain, mostly written by Matthew Newville'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

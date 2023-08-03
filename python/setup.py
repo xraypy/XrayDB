14678802e56b24b01dfcb46a14d9bab0fdebce82
Matthew Newville <newville@cars.uchicago.edu>
2023-08-03 12:21:52 -0500

    cleanup setup configs

--- python/setup.py
+++ python/setup.py
@@ -1,42 +1,5 @@
 #!/usr/bin/env python
-# import os
-# import sys
-#import shutil
 
-from setuptools import setup
-import setuptools_scm
-
+import setuptools
 if __name__ == '__main__':
-    setup(use_scm_version = {"root": ".."},
-           setup_requires=['setuptools_scm'])
-
-# dbfile = 'xraydb.sqlite'
-# matfile = 'materials.dat'
-#
-# dest_dbfile = os.path.join('xraydb', dbfile)
-# src_dbfile = os.path.join('..', dbfile)
-# if (not os.path.exists(dest_dbfile) and
-#     os.path.exists(src_dbfile)):
-#     shutil.copy(src_dbfile, dest_dbfile)
-#
-# desc="X-ray Reference Data for the Elements using SQLite"
-#
-# setup(name = 'xraydb',
-#       version=versioneer.get_version(),
-#       cmdclass=versioneer.get_cmdclass(),
-#       author='Matthew Newville',
-#       author_email='newville@cars.uchicago.edu',
-#       url='http://github.com/xraypy/XrayDB',
-#       download_url='http://github.com/xrapy/XrayDB',
-#       license='public domain',
-#       description=desc,
-#       long_description=desc,
-#       platforms=('Windows', 'Linux', 'Mac OS X'),
-#       classifiers=['Intended Audience :: Science/Research',
-#                    'Operating System :: OS Independent',
-#                    'Programming Language :: Python',
-#                    'Topic :: Scientific/Engineering'],
-#       install_requires=('sqlalchemy', 'numpy', 'scipy'),
-#       package_dir={'xraydb': 'xraydb'},
-#       packages=['xraydb'],
-#       package_data={'xraydb': [dbfile, matfile]})
+    setuptools.setup()

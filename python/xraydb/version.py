ce90c7b3850bd617362d44e3f5ba7f6be9512662
Matthew Newville <newville@cars.uchicago.edu>
2025-07-18 14:52:54 -0500

    update project file license and supported Python versions

--- python/xraydb/version.py
+++ python/xraydb/version.py
@@ -1,5 +1,5 @@
 """
 Version data - entered by hand ;(
 """
-__version__ = version = '4.5.7'
-__version_tuple__ = version_tuple = (4, 5, 7)
+__version__ = version = '4.5.8'
+__version_tuple__ = version_tuple = (4, 5, 8)
